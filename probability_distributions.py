d011ab8c4223bd0ec4af7e2bf1d584760abd7ec6
Tim Sotirhos <tim.sotirhos@gmail.com>
2019-10-03 11:29:33 -0500

    probability distribution exercises

--- probability_distributions.py
+++ probability_distributions.py
@@ -1,8 +1,10 @@
 # Probability Distribution
 import numpy as np
+import math
 from scipy import stats
 import matplotlib.pyplot as plt
 import viz
+import pandas as pd
 
 # 1.) A bank found that the average number of cars waiting during the noon hour at a drive-up window follows a Poisson distribution with a mean of 2 cars. 
 # Make a chart of this distribution and answer these questions concerning the probability of cars waiting at the drive-up window.
@@ -67,11 +69,11 @@ stats.binom(60,.01).pmf(1)
 n_students = round(.9*66)
 
 # cleanded every day
-stats.binom(n_students, .03).pmf(0)
+stats.binom(n_students, .03).sf(0)
 # not cleaned two day
-stats.binom(n_students, .03).pmf(0)
+stats.binom(n_students, .03).pmf(2)
 # not cleaned all week
-stats.binom(n_students, .03).pmf(0)
+stats.binom(n_students, .03).pmf(5)
 
 # 6.) You want to get lunch at La Panaderia, but notice that the line is usually very long at lunchtime. 
 # After several weeks of careful observation, 
@@ -79,21 +81,61 @@ stats.binom(n_students, .03).pmf(0)
 # If it takes 2 minutes for each person to order, and 10 minutes from ordering to getting your food, what is the likelihood that you have at least 15 minutes left to eat your food before you have to go back to class? 
 # Assume you have one hour for lunch, and ignore travel time to and from La Panaderia.
 
+mean = 15
+std= 3
+time_to_order = 2
+lunch_hour = 60
+eat_time = 15
+prep_time = 10
+wait_time = lunch_hour - mean * time_to_order - prep_time - eat_time
+people = round((lunch_hour - eat_time - prep_time) // time_to_order)
 
-# 7.) Connect to the employees database and find the average salary of current employees, along with the standard deviation. Model the distribution of employees salaries with a normal distribution and answer the following questions:
 
+lunch_line = stats.norm(15, 3).cdf(people)
+lunch_line
 
+# 7.) Connect to the employees database and find the average salary of current employees, along with the standard deviation. 
+# Model the distribution of employees salaries with a normal distribution and answer the following questions:
+
+def get_db_url(user, password, host, database_name):
+    url = f'mysql+pymysql://{user}:{password}@{host}/{database_name}'
+    return url
+
+import env
+
+from env import host, user, password
+
+url = get_db_url(user,password, host, "employees")
+
+url
+
+# Read the salaries tables into a dataframes
+
+salaries_query = "SELECT * FROM salaries WHERE to_date like '9999%%'"
+salaries = pd.read_sql(salaries_query, url) 
+(salaries.tail(25))
+
+average_salary = salaries.salary.mean()
+
+std_salary = salaries.salary.std()
+
+salary_distribution = stats.norm(average_salary, std_salary)
 
 # 7.a) What percent of employees earn less than 60,000?
 
-
+percent_under_60k = salary_distribution.cdf(60000)
 
 # 7.b) What percent of employees earn more than 95,000?
 
-
+percent_above_95k = salary_distribution.sf(95000)
 
 # 7.c) What percent of employees earn between 65,000 and 80,000?
 
+percent_above_65k = salary_distribution.sf(65000)
+percent_below_80k = salary_distribution.cdf(80000)
 
+percent_between_65k_and_80k = percent_below_80k - percent_above_65k
 
 # 7.d) What do the top 5% of employees make?
+
+top_5%_salary = salary_distribution.isf(.05)
